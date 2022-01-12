00294a6a34fc61cccac1d5a7720c0481b4b788f2
num4yo3 <82578883+num4yo3@users.noreply.github.com>
2022-01-12 16:26:15 +0900

    a

--- src/components/FigureContents.tsx
+++ src/components/FigureContents.tsx
@@ -1,76 +1,46 @@
+import { Plot } from "./Plot";
 import styled from "styled-components";
 
-const Scatter = styled.div`
-  position: absolute;
-  left: ${(props) => props.pos.x * 100 + "%"};
-  bottom: ${(props) => props.pos.y * 100 + "%"};
-  width: 69x;
-  height: 6px;
-  border-radius: 3px;
+const Wrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 95%;
+  height: 80%;
   background-color: rgb(255, 255, 255);
-  outline: solid 1px rgb(0, 0, 0);
-  transform: translateX(-3px) translateY(3px);
 `;
-type position = {
-  x: number;
-  y: number;
-};
-type range = {
-  xMin: number;
-  xMax: number;
-  yMin: number;
-  yMax: number;
-};
 
-const setScaleAuto = (poslist: position[]) => {
-  const posx: number[] = poslist.map((value) => value.x);
-  const posy: number[] = poslist.map((value) => value.y);
-  return {
-    xMin: Math.min(...posx),
-    xMax: Math.max(...posx),
-    yMin: Math.min(...posy),
-    yMax: Math.max(...posy)
-  };
-};
+const VAxis = styled.div`
+  width: 5%;
+  height: 90%;
+  background-color: rgb(240, 240, 240);
+`;
 
-const AdjustPos = (data: position[], range: range) => {
-  const xWidth: number = range.xMax - range.xMin;
-  const yWidth: number = range.yMax - range.yMin;
-  const modData: position[] = data.map((item) => ({
-    x: (item.x - range.xMin) / xWidth,
-    y: (item.y - range.yMin) / yWidth
-  }));
-  return modData;
-};
+const PlotBox = styled.div`
+  width: 95%;
+  height: 90%;
+`;
 
-export const Plot = () => {
-  const data: position[] = [
-    { x: 0, y: 0 },
-    { x: 1, y: 1 },
-    { x: 2, y: 4 },
-    { x: 3, y: 9 },
-    { x: 4, y: 16 },
-    { x: 5, y: 25 },
-    { x: 6, y: 36 },
-    { x: 7, y: 49 },
-    { x: 8, y: 64 },
-    { x: 9, y: 81 }
-  ];
-  const range: range = setScaleAuto(data);
-  const modData: position[] = AdjustPos(data, range);
+const Space = styled.div`
+  width: 5%;
+  height: 10%;
+  background-color: rgb(240, 240, 240);
+`;
 
+const HAxis = styled.div`
+  width: 95%;
+  height: 10%;
+  background-color: rgb(240, 240, 240);
+`;
+
+export const FigureContents = () => {
   return (
-    <div
-      style={{
-        position: "relative",
-        width: "100%",
-        height: "100%",
-        outline: "solid 1px rgb(100,100,100)"
-      }}
-    >
-      {modData.map((item, index) => (
-        <Scatter key={index} pos={{ ...item }} />
-      ))}
-    </div>
+    <Wrapper>
+      <VAxis />
+      <PlotBox>
+        <Plot />
+      </PlotBox>
+      <Space />
+      <HAxis />
+    </Wrapper>
   );
 };
